9a81c4dd3249f1244cc41fae25ef11fc50ac731e
jordsek <73316853+jordsek@users.noreply.github.com>
2024-07-09 13:52:18 +0100

    First commit

---
+++ ChatMate/ChatMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMateApp.swift
+//  ChatMate
+//
+//  Created by Jor . on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
